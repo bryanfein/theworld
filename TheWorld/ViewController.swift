c781a93c39fc49eca6376114ce52336cbffab48a
bryanfein <bryanfein@gmail.com>
2017-03-27 16:55:43 -0400

    Initial Commit

---
+++ TheWorld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TheWorld
+//
+//  Created by Bryan Fein on 3/27/17.
+//  Copyright © 2017 Bryan Fein. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
